b7c5e8e09b3c7f480638a05b77dcbdbef2be9c01
Benjamin Denzler <bdenz001@ucr.edu>
2022-10-07 17:31:40 -0700

    Formatted code

--- client.c
+++ client.c
@@ -9,44 +9,57 @@
 #include <errno.h>
 #include <arpa/inet.h>
  
-int main(void)
-{
-  int sockfd = 0,n = 0;
+int main(int argc, char * argv[]) {
+  int sockfd = 0, n = 0;
   char recvBuff[1024];
+  char * hi = "Hi!";
+  char * bye = "Bye!";
+
+  // Info that the socket will be bound to
   struct sockaddr_in serv_addr;
- 
-  memset(recvBuff, '0' ,sizeof(recvBuff));
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
-    {
-      printf("\n Error : Could not create socket \n");
-      return 1;
-    }
- 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(5000);
-  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 
-  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
-    {
-      printf("\n Error : Connect Failed \n");
-      return 1;
-    }
- 
-  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
-      recvBuff[n] = 0;
-      if(fputs(recvBuff, stdout) == EOF)
-    {
+
+  // Set receive buffer to all 0s
+  memset(recvBuff, '0', sizeof(recvBuff));
+
+  // Set sockfd as our socket
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Error : Could not create socket \n");
+    return 1;
+  }
+
+  // Set port etc. for the socket
+  serv_addr.sin_family = AF_INET; // Address family
+  serv_addr.sin_port = htons(5000); // Port number
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP address
+
+  // Try to connect to socket
+  if (connect(sockfd, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\n Error : Connect Failed \n");
+    return 1;
+  }
+
+  if (argc == 2 && argv[1] == 1) {
+    printf( % s, hi);
+  } else if (argc == 2 && argv[1] == 2) {
+    printf( % s, bye);
+  } else {
+	printf("Nothing!");
+  }
+
+  // Read a # of chars equal to the size of recvBuff from the socket (n)
+  while ((n = read(sockfd, recvBuff, sizeof(recvBuff) - 1)) > 0) {
+    // Set last byte to 0
+    recvBuff[n] = 0;
+    // Output the contents of the recvBuff
+    if (fputs(recvBuff, stdout) == EOF) {
       printf("\n Error : Fputs error");
     }
-      printf("\n");
-    }
- 
-  if( n < 0)
-    {
-      printf("\n Read Error \n");
-    }
- 
- 
+    printf("\n");
+  }
+
+  if (n < 0) {
+    printf("\n Read Error \n");
+  }
+
   return 0;
 }
--- server.c
+++ server.c
@@ -8,45 +8,51 @@
 #include <string.h>
 #include <sys/types.h>
  
-int main(void)
-{
-  int listenfd = 0,connfd = 0;
-  
+int main(void) {
+  int listenfd = 0, connfd = 0;
+
+  // Info that the socket will be bound to
   struct sockaddr_in serv_addr;
- 
-  char sendBuff[1025];  
-  int numrv;  
- 
+
+  char sendBuff[1025];
+  int numrv;
+
+  // Set listenfd as our socket
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   printf("socket retrieve success\n");
-  
-  memset(&serv_addr, '0', sizeof(serv_addr));
+
+  // Clear the socket info and send buffer so we don't send null
+  memset( & serv_addr, '0', sizeof(serv_addr));
   memset(sendBuff, '0', sizeof(sendBuff));
-      
-  serv_addr.sin_family = AF_INET;    
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-  serv_addr.sin_port = htons(5000);    
- 
-  bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
-  
-  if(listen(listenfd, 10) == -1){
-      printf("Failed to listen\n");
-      return -1;
+
+  // Set port etc. for the socket
+  serv_addr.sin_family = AF_INET; // Address family
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // IP address
+  serv_addr.sin_port = htons(5000); // Port number
+
+  // Bind the server's socket to the info in serv_addr
+  bind(listenfd, (struct sockaddr * ) & serv_addr, sizeof(serv_addr));
+
+  // Start listening to the socket
+  if (listen(listenfd, 10) == -1) {
+    printf("Failed to listen\n");
+    return -1;
   }
-     
-  
-  while(1)
-    {
-      
-      connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
-  
-      strcpy(sendBuff, "Message from server33");
-      write(connfd, sendBuff, strlen(sendBuff));
- 
-      close(connfd);    
-      sleep(1);
-    }
- 
- 
+
+  while (1) {
+    // Accept any requests from the client, creating new socket connfd
+    connfd = accept(listenfd, (struct sockaddr * ) NULL, NULL);
+
+    // Copy message to sendBuff
+    strcpy(sendBuff, "Message from server33");
+
+    // Send message in sendBuff over socket connfd to client
+    write(connfd, sendBuff, strlen(sendBuff));
+
+    // Close the connection
+    close(connfd);
+    sleep(1);
+  }
+
   return 0;
 }
