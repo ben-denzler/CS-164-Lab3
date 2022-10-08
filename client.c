8461340b9e853752051e8f878f168217d93739e3
Benjamin Denzler <bdenz001@ucr.edu>
2022-10-07 17:58:12 -0700

    Testing send

--- client.c
+++ client.c
@@ -12,8 +12,8 @@
 int main(int argc, char * argv[]) {
   int sockfd = 0, n = 0;
   char recvBuff[1024];
-  char * hi = "Hi!";
-  char * bye = "Bye!";
+  char* hi = "Hi!";
+  char* bye = "Bye!";
 
   // Info that the socket will be bound to
   struct sockaddr_in serv_addr;
@@ -32,14 +32,6 @@ int main(int argc, char * argv[]) {
   serv_addr.sin_port = htons(5000); // Port number
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP address
 
-  if (argc == 2 && *argv[1] == '1') {
-    printf("%s", hi);
-  } else if (argc == 2 && *argv[1] == '2') {
-    printf("%s", bye);
-  } else {
-	  printf("Nothing!");
-  }
-
   // Try to connect to socket
   if (connect(sockfd, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0) {
     printf("\n Error : Connect Failed \n");
@@ -48,13 +40,21 @@ int main(int argc, char * argv[]) {
 
   // Read a # of chars equal to the size of recvBuff from the socket (n)
   while ((n = read(sockfd, recvBuff, sizeof(recvBuff) - 1)) > 0) {
-    // Set last byte to 0
-    recvBuff[n] = 0;
-    // Output the contents of the recvBuff
-    if (fputs(recvBuff, stdout) == EOF) {
-      printf("\n Error : Fputs error");
+    if (argc == 2 && *argv[1] == '1') {
+      send(sockfd, hi, strlen(hi), 0);
+      printf("\"Hi!\" was sent to the server.\n");
+    } else if (argc == 2 && *argv[1] == '2') {
+      send(sockfd, bye, strlen(bye), 0);
+      printf("\"Bye!\" was sent to the server.\n");
+    } else {
+      // Set last byte to 0
+      recvBuff[n] = 0;
+      // Output the contents of the recvBuff
+      if (fputs(recvBuff, stdout) == EOF) {
+        printf("\n Error : Fputs error");
+      }
+      printf("\n");
     }
-    printf("\n");
   }
 
   if (n < 0) {
