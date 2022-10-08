7955be907158394049067009c914448181ebcbab
ben-denzler <bdenz001@ucr.edu>
2022-10-08 13:49:14 -0700

    Fixed bug

--- client.c
+++ client.c
@@ -20,7 +20,7 @@ int main(int argc, char * argv[]) {
 
   if (argc == 3) {
     hostname = argv[2];
-    printf("Grabbed hostname from command-line argument.\n")
+    printf("Grabbed hostname from command-line argument.\n");
   }
 
   // Info that the socket will be bound to
